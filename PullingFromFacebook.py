d66a0284d85d63fdd456bd3678c61f7b3bbf02ba
jenwei <jwei@ubuntu.(none)>
2014-03-02 20:07:45 -0500

    Working code.

--- PullingFromFacebook.py
+++ PullingFromFacebook.py
@@ -2,7 +2,7 @@
 """
 Created on Wed Feb 26 22:28:21 2014
 
-@author: charliemouton and Jenniferwei
+@author: CharlieMouton and JenniferWei
 
 Charlie's fb code: CAAEuAis8fUgBANEcpy6pnuIwr8IZBLFo3MT7N2m2xbgkecbZCps2CTbm3OY
 fN9nbLZAS8ZBQaVzqGPMcfP3p4Dma1eH2YRvNGxGLr4xzPuSpZBsYAVi4BMPUp7HTaNkbNddDEZC9Z
@@ -14,33 +14,60 @@ Blt5zC3bf9HICmYAJFPZC7mU4zQFHksF1eTRlf9E2RpypFhZBJ3tqkYYCKwZD
 from pattern.web import *
 f = Facebook(license='CAAEuAis8fUgBANEcpy6pnuIwr8IZBLFo3MT7N2m2xbgkecbZCps2CTbm3OYfN9nbLZAS8ZBQaVzqGPMcfP3p4Dma1eH2YRvNGxGLr4xzPuSpZBsYAVi4BMPUp7HTaNkbNddDEZC9ZBlt5zC3bf9HICmYAJFPZC7mU4zQFHksF1eTRlf9E2RpypFhZBJ3tqkYYCKwZD')
 me = f.profile()
-print me
+print me #prints out information of license (id,name,birthday,gender,country)
 counter = 0
-my_friends = f.search(me[0], type=FRIENDS, count=100)
+textDict = {}
+textFreq = {}
+my_friends = f.search(me[0], type=FRIENDS, count=1000)
+
 
 def access_newsfeed(frnd):
     """
-    WRITE A DOCSTRING HERE
+    access_newsfeed takes in the information of a friend, accesses their 
+    newsfeed, and puts all of it into a dictionary, where the key is the 
+    name and the value is the newsfeed text list.
+    
+    frnd: is the newsfeed information for a specified friend
+    
+    returns: value (list of newsfeed text) from the textDictionary for that
+    specific friend
     """
+    textList = []
     friend_news = f.search(frnd.id, type=NEWS, count=100)
     for news in friend_news:
-        print news.text
-        print type(news.author)
-        print news.date
+        #print news.text
+        #print type(news.author)
+        #print news.date
+        textList.append(news.text)
+    textDict[frnd.author[1]] = textList
+    return textDict[frnd.author[1]]
+    
 
-for friend in my_friends:
-    temp_name = friend.author[1]
-    #print friend.author[1] #Prints the name of the author of the post
+def facebook_pull(specific_friend):
+    """
+    facebook_pull pulls the newsfeed information for a specific friend.
+    specific_friend: name of specific friend as a string
     
-    if (temp_name == "Tim Mouton"):
-        access_newsfeed(friend)
+    returns: dictionary of posts from the newsfeed of the specific friend
+    """
     
-    #print friend_news
-    #for news in friend_news:
-    #    counter += 1
-    #    print news.text
-    #    print news.author[1]
-    #    print type(news.author)
-    #    print news.date
-    #    print counter
-    
+    for friend in my_friends:
+        temp_name = friend.author[1]
+        #print friend.author[1] #Prints the name of the author of the post
+        
+        if (temp_name == specific_friend):
+            individual_textDict = access_newsfeed(friend)
+            return individual_textDict
+        #print friend_news
+        #for news in friend_news:
+        #    counter += 1
+        #    print news.text
+        #    print news.author[1]
+        #    print type(news.author)
+        #    print news.date
+        #    print counter
+
+
+#test for facebook_pull
+print facebook_pull("Danielle Mouton")
+print facebook_pull("Casey Alvarado")
